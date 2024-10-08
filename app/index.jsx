b4c656c1506f4c5bf2189bb2a2da38506807512e
Rafael Santos <rafaelsantost87@gmail.com>
2024-10-08 16:35:07 -0300

    bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ const index = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className='text-3xl font-pblack'>Aora</Text>
-      <Link href='/profile' style={{color:'blue'}}>Go to Profile</Link>
+      <Link href='/home' style={{color:'blue'}}>Go to Home</Link>
     </View>
   )
 }
